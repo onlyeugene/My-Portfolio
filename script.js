9e76c46e6d696deb50148b610210ced9ac7b14b6
onlyeugene <eugeneokogun@gmail.com>
2024-03-03 13:37:25 +0100

    made changes to the frontend development text incorporating javascript

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu(){
     menu.classList.toggle('open')
     icon.classList.toggle('open')
 }
+
+const typed = new Typed ('.multiple-text', {
+    strings: ['Frontend Developer','Help My Life 🥲!'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+})
+
+ScrollReveal().reveal({
+    distance: '1000px',
+    duration: 2000,
+    delay: 200,
+});
+
+// ScrollReveal().reveal('.section__pic-container,.section__text', {origin:'right'})
